1cd7b6ec35f59df634d9fed3e186ac9be8d7d1ef
Kaushal Poojary <kaushalpoojary2002@gmail.com>
2022-08-17 00:06:29 +0530

    Developed contact page

---
+++ src/Landing pages/Contact.jsx
@@ -0,0 +1,240 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+// Components import here
+import Navbar from "../Landing pages/Components/Navbar";
+import SocialMedia from "../Landing pages/Components/Social-media";
+
+// Assets import here
+import image from "../Landing pages/images/contact-us-image.jpg";
+
+const Image = styled.img`
+  width: 100%;
+`;
+
+const Form = styled.form`
+  background-color: white;
+  width: 45%;
+  display: flex;
+  flex-direction: column;
+  padding: 27px 22px;
+  border-radius: 10px;
+`;
+
+const SubmitButton = styled.button`
+  background-color: #f7f1e3;
+  color: black;
+  border-radius: 5px;
+  border: none;
+  width: 120px;
+  height: 40px;
+  align-self: center;
+  margin-top: 20px;
+  cursor: pointer;
+  font-family: "Forum", cursive;
+  transition: 0.3s ease-in-out;
+  :hover,
+  focus {
+    box-shadow: 0 0.5em 0.5em -0.4em var(--hover);
+    transform: translateY(-0.25em);
+    background-color: orange;
+  }
+    font-size: 20px;
+    font-weight: bold;
+  }
+`;
+
+const Input = styled.input`
+  background-color: #f7f1e3;
+  margin-bottom: 24px;
+  border: none;
+  border-radius: 5px;
+  padding: 8px 16px;
+  ::placeholder {
+    font-family: "Forum", cursive;
+    font-size: 16px;
+  }
+`;
+
+const Textarea = styled.textarea`
+  background-color: #f7f1e3;
+  border: none;
+  border-radius: 7px;
+  padding: 8px 16px;
+  ::placeholder {
+    font-family: "Forum", cursive;
+    font-size: 16px;
+  }
+`;
+
+const FormWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+  width: 50%;
+  position: absolute;
+  top: 200px;
+  right: 8%;
+`;
+
+const Select = styled.select`
+  background-color: #f7f1e3;
+  margin-bottom: 24px;
+  border-radius: 5px;
+  padding: 8px 12px;
+  border: none;
+  font-size: 16px;
+  font-family: "Forum", cursive;
+`;
+
+const ContactWrap = styled.div`
+  position: relative;
+  font-family: "Forum", cursive;
+`;
+
+const Reach = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 70px;
+  background-color: #ebe6e6;
+  margin-top: -4px;
+`;
+
+const Text = styled.p`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 55px;
+  line-height: 83px;
+  margin: 0;
+`;
+
+const Mail = styled.a`
+  text-decoration: none;
+  color: #6f6c6c;
+  margin-top: 19px;
+`;
+
+const Email = styled.text`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 32px;
+  line-height: 55px;
+`;
+
+const LocationWrap = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Location = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 27%;
+  padding: 30px;
+`;
+
+const Branch = styled.text`
+  font-style: normal;
+  font-weight: 400;
+  font-size: 35px;
+  line-height: 39px;
+  text-align: center;
+  margin-bottom: 30px;
+  margin-top: 15px;
+`;
+
+const Address = styled.text`
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 22px;
+  text-align: center;
+`;
+
+const Contact = ({ text = "Mumbai" }) => {
+  const [searchString, setSearchString] = useState();
+  const [email, setEmail] = useState();
+  const [query, setQuery] = useState();
+
+  return (
+    <ContactWrap>
+      <Navbar />
+      <Image src={image} alt="No image" />
+      <FormWrap>
+        <Form>
+          <Select>
+            <option value="default" hidden style={{ color: "#454545" }}>
+              How can we help you*
+            </option>
+            <option value="help">I need help with my order</option>
+            <option value="outdated">
+              I found incorrect/outdated information on a page
+            </option>
+            <option value="not-working">
+              The website is not working the way it should
+            </option>
+            <option value="feedback">
+              I would like to give feedback/suggestions
+            </option>
+            <option value="other">Other</option>
+          </Select>
+          {/* Name */}
+          <Input
+            type="text"
+            value={searchString}
+            onChange={(e) => setSearchString(e.target.value)}
+            placeholder="Name*"
+          />
+          {/* Email */}
+          <Input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email*"
+          />
+          {/* Query */}
+          <Textarea
+            rows={6}
+            cols={50}
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            placeholder="Type Text Here*"
+          />
+          <SubmitButton>Submit</SubmitButton>
+        </Form>
+      </FormWrap>
+      <Reach>
+        <Text>You can also reach out to us on:</Text>
+        <SocialMedia />
+        <Text style={{ marginTop: 40 }}>or mail us at:</Text>
+        <Mail href="mailto:2020.shaun.dsouza@ves.ac.in">
+          <Email>2020.shaun.dsouza@ves.ac.in</Email>
+        </Mail>
+        <Mail href="mailto:2020.abhinav.haridas@ves.ac.in">
+          <Email>2020.abhinav.haridas@ves.ac.in</Email>
+        </Mail>
+        <Text style={{ marginTop: 40 }}>we are located at:</Text>
+        <LocationWrap>
+          <Location>
+            <Branch>Headquarters</Branch>
+            <Address>
+              Level 7, Plot no, FIFC Building, C-54&55, G Block BKC, Bandra
+              East, Mumbai, Maharashtra 400098
+            </Address>
+          </Location>
+          <Location>
+            <Branch>{text} Branch</Branch>
+            <Address>
+              Level 7, Plot no, FIFC Building, C-54&55, G Block BKC, Bandra
+              East, Mumbai, Maharashtra 400098
+            </Address>
+          </Location>
+        </LocationWrap>
+      </Reach>
+    </ContactWrap>
+  );
+};
+
+export default Contact;
