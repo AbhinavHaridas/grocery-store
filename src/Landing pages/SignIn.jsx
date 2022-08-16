613ce0a9cf725f27415947e0680ac63515470017
“himnish” <himnish54@gmail.com>
2022-08-16 14:01:45 +0530

    worked on modal(fail)

--- src/Landing pages/SignIn.jsx
+++ src/Landing pages/SignIn.jsx
@@ -1,9 +1,16 @@
-import React from "react";
-
+import React, { useState } from 'react';
+import Modal from 'react-modal'
 const SignIn = () => {
     return (
         <div>
-
+<Modal isOpen={true}>
+    <form>
+    <center> <h1 >Welcome,Again</h1> </center>
+    <label> Phone no/email </label>
+<input type="text"
+required />
+        </form> 
+     </Modal>
         </div>
     )
 }
