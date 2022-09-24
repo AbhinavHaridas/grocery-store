6df8fcdc45c749b7b7a5e97a50863c310d706351
Abhinav <abhikeabhi2002@gmail.com>
2022-09-25 02:20:09 +0530

    Update CartItem.jsx
    60% of the page is done

--- src/Home pages/Components/CartItem.jsx
+++ src/Home pages/Components/CartItem.jsx
@@ -13,6 +13,8 @@ const TextP = styled.p`
 font-family: 'Forum', cursive; 
 font-size: 3vh;
 color: black;  
+margin-left: 20px;
+margin-right: 20px
 `;
 
 const Amount = styled.div`
@@ -25,11 +27,8 @@ const Amount = styled.div`
     justify-content: space-around;
     align-items: center;
     margin-top: 1.8vh;
-`
-
-const DisplayEnd = styled.div `
-    display: flex;
-    flex-direction: row;
+    margin-left: 20px;
+    margin-right: 20px
 `
 
 const CartItem = () => {
@@ -37,35 +36,61 @@ const CartItem = () => {
 
     return (
         <div style={{
-            width: '150vh', 
+            width: '130vh', 
             height: '25vh', 
             backgroundColor: 'white',
             display: 'flex',
             flexDirection: 'row',
             marginTop: '2vh',
-            borderRadius: '2px'
+            borderRadius: '6px'
             }}>
                 <img src='https://imgur.com/SDCKI4n.png' alt='none' />
-                <div style={{display: 'flex', flexDirection: 'column'
-            }}>
-
-                </div>
-                <div style = {{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'space-around',
-                    width: '100%',
-                }}>
-                    <TextP>Nagpur Oranges</TextP>
-                    <TextP>$40.99</TextP>
-                </div>
                 <div style={{
+                display: 'flex', 
+                flexDirection: 'column',
+                width: '100%'
+                }}>
+                    <div style = {{
                     display: 'flex',
                     flexDirection: 'row',
+                    justifyContent: 'space-between',
                     width: '100%',
+                    }}>
+                        <TextP>Nagpur Oranges</TextP>
+                        <TextP>$40.99</TextP>
+                    </div>
+                    <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
                     justifyContent: 'flex-start',
                     }}>
                         <TextC>Qty: 1</TextC>
+                    </div>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        alignItems: 'center'
+                    }}>
+                        <Amount>
+                            <TextC>1kg</TextC>
+                            <i class="fa-solid fa-caret-down fa-2xl" style={{marginLeft: '2vh'}}></i>
+                        </Amount>
+                        <div style={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center'
+                        }}>
+                        <i class="fa-solid fa-trash-can fa-2xl" style={{marginTop: '1.5vh', marginRight: '4vh'}}></i>
+                        <Amount>
+                            <i class="fa-solid fa-plus" onClick={() => setCount(count + 1)}></i>
+                            <TextC>{count}</TextC>
+                            <i class="fa-solid fa-minus" onClick={() => {
+                                if (count !== 0) setCount(count - 1)
+                            }}></i>
+                        </Amount>
+                        </div>
+                    </div>
                 </div>
 
         </div>
