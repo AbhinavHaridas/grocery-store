044c3562e1080cf29bc2f4a1de569be05410a4f7
Kaushal Poojary <kaushalpoojary2002@gmail.com>
2022-10-06 00:22:16 +0530

    Some commit

--- src/Home pages/Components/TrackOrder.jsx
+++ src/Home pages/Components/TrackOrder.jsx
@@ -138,7 +138,7 @@ const TrackOrder = () => {
         <Heading>Track Your Order</Heading>
 
         <Content>
-          <LoadScript googleMapsApiKey="AIzaSyBYMVLPoeReMN4jWOhKCV-g6x0BhYeXPus">
+          <LoadScript googleMapsApiKey={`${process.env.API_KEY}`}>
             <GoogleMap
               mapContainerStyle={containerStyle}
               center={center}
