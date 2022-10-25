3767db7e72147f4cccc17bed8e463faa3675bc62
himnishisrani <110344358+himnishisrani@users.noreply.github.com>
2022-10-26 01:14:27 +0530

    hjkjl

--- src/Home pages/Components/Modalordersummary.jsx
+++ src/Home pages/Components/Modalordersummary.jsx
@@ -1,4 +1,4 @@
-import React,{useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import "../../Home pages/Styles/Ordersummary.css"
 
 const modal = document.querySelector('#my-modal');
@@ -22,58 +22,59 @@ function outsideClick(e) {
 
 const Modal_a = ({ isOpen, toggle }) => {
   const [open, setOpen] = useState(false);
-  return(
-    
-    <div className="ordersum" align="center">
-     <button id="modal-btn" onClick={() => setOpen(true)} class="button">Click Here</button>
-     open&&(
-<div id="my-modal" class="modal">
-  <div class="modal-content">
-    <div class="modal-header">
-      <h2>Order Summary</h2>
-    </div>
-    <div class="modal-body">
-      <p class="orderitem" align="left">Order Items</p>
-      <table class="table_summary">
-  <tr>
-    <th>Firstname</th>
-    <th>Lastname</th>
-    <th>Savings</th>
-  </tr>
-  <tr>
-    <td>Peter</td>
-    <td>Griffin</td>
-    <td>$100</td>
-  </tr>
-  <tr>
-    <td>Lois</td>
-    <td>Griffin</td>
-    <td>$150</td>
-  </tr>
-  <tr>
-    <td>Joe</td>
-    <td>Swanson</td>
-    <td>$300</td>
-  </tr>
-  <tr>
-    <td>Cleveland</td>
-    <td>Brown</td>
-    <td>$250</td>
-</tr>
-</table>
+  return (
 
-<p></p>
+    <div class="ordersum" align="center" >
+      <button id="modal-btn" onClick={() => setOpen(true)} class="button">Click Here</button>
+      {open && (
+        <div id="my-modal" class="modal" onClick={() => setOpen(false)}>
+          <div class="modal-content">
+            <div class="modal-header">
+              <h2>Order Summary</h2>
+            </div>
+            <div class="modal-body">
+              <p class="orderitem" align="left">Order Items</p>
+              <table class="table_summary">
+                <tr>
+                  <th>Firstname</th>
+                  <th>Lastname</th>
+                  <th>Savings</th>
+                </tr>
+                <tr>
+                  <td>Peter</td>
+                  <td>Griffin</td>
+                  <td>$100</td>
+                </tr>
+                <tr>
+                  <td>Lois</td>
+                  <td>Griffin</td>
+                  <td>$150</td>
+                </tr>
+                <tr>
+                  <td>Joe</td>
+                  <td>Swanson</td>
+                  <td>$300</td>
+                </tr>
+                <tr>
+                  <td>Cleveland</td>
+                  <td>Brown</td>
+                  <td>$250</td>
+                </tr>
+              </table>
+
+              <p></p>
+            </div>
+            <div class="detail-modal">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla repellendus nisi, sunt consectetur ipsa velit
+                repudiandae aperiam modi quisquam nihil nam asperiores doloremque mollitia dolor deleniti quibusdam nemo
+                commodi ab</p>
+            </div>
+
+          </div>
+        </div>
+      )}
     </div>
-    <div class="detail-modal">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla repellendus nisi, sunt consectetur ipsa velit
-        repudiandae aperiam modi quisquam nihil nam asperiores doloremque mollitia dolor deleniti quibusdam nemo
-        commodi ab</p>
-    </div>
-    
-  </div>
-</div>
-   </div>
-))
+  )
 };
 
 export default Modal_a;
